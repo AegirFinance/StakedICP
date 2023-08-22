9d5043d16efd11458aabffa7ff9b6c86f7be3155
0xAegir <99401181+0xAegir@users.noreply.github.com>
2023-08-22 08:06:18 +0100

    Remove bignumber dep

--- src/website/src/components/StakeForm/Price.tsx
+++ src/website/src/components/StakeForm/Price.tsx
@@ -1,4 +1,3 @@
-import { BigNumber } from '@ethersproject/bignumber';
 import axios from 'axios';
 import React from 'react';
 import {
--- src/website/src/components/StakeForm/StakePanel.tsx
+++ src/website/src/components/StakeForm/StakePanel.tsx
@@ -1,4 +1,3 @@
-import { BigNumber } from "@ethersproject/bignumber";
 import React from 'react';
 import {
     ConfirmationDialog,
